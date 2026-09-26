e2d16c04d90d98999d943a9043b8c6613ab11f6a
aucikkosmonaucik-boop <aucikkosmonaucik@gmail.com>
2026-09-26 09:55:57 +0200

    Remove location filter pill from header next to Czytella logo

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/czytella_provider.dart';
-import '../widgets/location_filter_sheet.dart';
 import 'listings_view.dart';
 import 'my_shelf_view.dart';
 import 'isbn_scanner_view.dart';
@@ -113,43 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ],
                               ),
                             ),
-                            const SizedBox(width: 12),
-
-                            // Location Chip
-                            InkWell(
-                              onTap: () => LocationFilterSheet.show(context),
-                              borderRadius: BorderRadius.circular(20),
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 4),
-                                decoration: BoxDecoration(
-                                  color: Colors.green.shade50,
-                                  borderRadius: BorderRadius.circular(20),
-                                  border:
-                                      Border.all(color: Colors.green.shade200),
-                                ),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(Icons.location_on,
-                                        size: 13, color: Colors.green.shade800),
-                                    const SizedBox(width: 3),
-                                    Text(
-                                      provider.currentCity,
-                                      style: TextStyle(
-                                        fontSize: 11,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.green.shade900,
-                                      ),
-                                    ),
-                                    const Icon(Icons.arrow_drop_down,
-                                        size: 15, color: Colors.green),
-                                  ],
-                                ),
-                              ),
-                            ),
-
-                            const SizedBox(width: 12),
+                            const SizedBox(width: 16),
 
                             // Right-aligned Navigation & Action controls
                             Expanded(
